ca17dcecaf519c3ddd590610e462aac465de9a45
ar.chusovitin <ar.chusovitin@tensor.ru>
2022-04-24 17:26:37 +0700

    * Поправил структуру

--- game/sql/event_to_game.sql
+++ game/sql/event_to_game.sql
@@ -5,9 +5,12 @@ CREATE SEQUENCE IF NOT EXISTS event_to_game_id_event_seq START 1;
 
 CREATE TABLE IF NOT EXISTS public.event_to_game
 (
-  id_event integer NOT NULL DEFAULT nextval('event_to_game_id_event_seq'::regclass),
+
+  id_event_to_game integer NOT NULL DEFAULT nextval('event_to_game_id_event_seq'::regclass),
+  id_event integer,
   id_game integer,
   round integer
+  CONSTRAINT event_pk PRIMARY KEY (id_event_to_game)
 )
 WITH (
   OIDS=FALSE
