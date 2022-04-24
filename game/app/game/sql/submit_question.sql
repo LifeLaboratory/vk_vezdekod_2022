667fb57d8250ad7a23447b6f037133898d07d469
ar.chusovitin <ar.chusovitin@tensor.ru>
2022-04-24 17:38:40 +0700

    * Правка Sql скрипта

--- game/app/game/sql/submit_question.sql
+++ game/app/game/sql/submit_question.sql
@@ -1,7 +1,7 @@
 select
-  (q.answer -> {answer}::text ->> 'health')::int health,
-  (q.answer -> {answer}::text ->> 'point')::int point,
-  (q.answer -> {answer}::text ->> 'money')::int money,
+  (q.answer::json -> {answer}::text ->> 'health')::int health,
+  (q.answer::json -> {answer}::text ->> 'point')::int point,
+  (q.answer::json -> {answer}::text ->> 'money')::int money,
   e.round event_round,
   g.round cur_round,
   g.id_game,
@@ -9,15 +9,15 @@ select
   g.id_question,
   g.id_user,
   q.good_answer,
-  ((q.answer -> q.good_answer::text) ->> 'description')::text description
+  ((q.answer::json -> q.good_answer::text) ->> 'description')::text description
 from
   game g
 
 join question q on q.id_question = g.id_question
-left join event e on e.id_event = (q.answer -> 0::text ->> 'id_event')::int
+left join event e on e.id_event = (q.answer::json -> 0::text ->> 'id_event')::int
 /*left join lateral (
       select
-  ((q.answer -> q.good_answer::text) ->> 'description')::int description
+  ((q.answer::json -> q.good_answer::text) ->> 'description')::int description
       ) lt on true*/
 where
   id_user = {id_user}
