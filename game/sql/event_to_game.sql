42078a3dc02ebaf2018899290d46817a28de74c9
ar.chusovitin <ar.chusovitin@tensor.ru>
2022-04-24 17:16:35 +0700

    * Нет инициализации таблицы

---
+++ game/sql/event_to_game.sql
@@ -0,0 +1,18 @@
+-- Table: public.event_to_game
+
+-- DROP TABLE public.event_to_game;
+CREATE SEQUENCE IF NOT EXISTS event_to_game_id_event_seq START 1;
+
+CREATE TABLE IF NOT EXISTS public.event_to_game
+(
+  id_event integer NOT NULL DEFAULT nextval('event_to_game_id_event_seq'::regclass),
+  id_game integer,
+  round integer
+  CONSTRAINT event_pk PRIMARY KEY (id_event)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.event_to_game
+  OWNER TO postgres;
+
