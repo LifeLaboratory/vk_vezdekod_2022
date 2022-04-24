9d302dc4f90b2a3dbf8bbf36964666e1c3c634cc
ar.chusovitin <ar.chusovitin@tensor.ru>
2022-04-24 17:33:33 +0700

    * Фикс базы

--- game/sql/person.sql
+++ game/sql/person.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS public.person
   description text,
   pic text,
   health double precision,
-  food double precision,
-  leisure double precision,
+  point double precision,
+  money double precision,
   communication double precision,
   value integer,
   CONSTRAINT person_pk PRIMARY KEY (id_person)
@@ -30,6 +30,6 @@ CREATE UNIQUE INDEX IF NOT EXISTS person_id_person_uindex
   USING btree
   (id_person);
 
-INSERT INTO "person" ("id_person", "name", "description", "pic", "health", "food", "leisure", "communication", "value") VALUES
+INSERT INTO "person" ("id_person", "name", "description", "pic", "health", "point", "money", "communication", "value") VALUES
 (2,	'Программист',	'Пишу бэки на Python, фронты на React, поднимаю на k8s и точно знаю как вести себя в интернете!',	'https://i1.wp.com/s3.amazonaws.com/gymndz/wp-content/uploads/2018/04/%D1%801.png?ssl=1',	75,	150,	300,	200,	205),
 (1,	'Студент',	'Я только учусь, но уже разбираюсь во всех интернетах!',	'https://st.depositphotos.com/3526225/5170/v/600/depositphotos_51707511-stock-illustration-student-flat-illustration-with-icons.jpg',	100,	50,	150,	100,	100);
